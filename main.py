31239be794369cce12927a5c15e7ec096ded317f
N1304095 <beneetta.binoy2024@my.ntu.ac.uk>
2025-09-12 12:36:35 +0100

    created a grid

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ pygame.init()
 screen = pygame.display.set_mode(size=(500, 500))
 pygame.display.set_caption("Tic-Tac-Toe")
 running = True
+BLACK = (0, 0, 0)
+
+def drawGrid():
+    blockSize = 100
+    for x in range(0, 500, blockSize):
+        for y in range(0, 500, blockSize):
+            square = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(screen, BLACK, square, 1)
 
 while running:
+    screen.fill("grey")
+    drawGrid()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    screen.fill("grey")
 
     pygame.display.flip()
     
